a80266c06fcc9876cb71e75b5a07437743047244
Your Name <youremail@example.com>
2025-02-18 17:29:26 +0500

    database

--- config/database.js
+++ config/database.js
@@ -3,16 +3,32 @@ const mongoose = require('mongoose');
 // const MONGO_URL = "mongodb://localhost:27017/Recipe_Sharing"; // Localhost
 const MONGO_URL = "mongodb+srv://harisdevcodes:NnzlJjJUXEIozSVP@muhammadharis.8qkyi.mongodb.net/";
 
+// const mongoConnect = async (callback) => {
+//     try{
+//         await mongoose.connect(MONGO_URL); 
+//         console.log('Connected to MongoDB with Mongoose');
+//         callback();
+//     }
+//     catch(err){
+//         console.log("MongoDB connection failded: ", err);
+//     }
+// }
+let isConnected = false;
+
 const mongoConnect = async (callback) => {
-    try{
-        await mongoose.connect(MONGO_URL); 
+    if (isConnected) {
+        console.log('Using existing connection');
+        return callback();
+    }
+    try {
+        await mongoose.connect(MONGO_URL);
+        isConnected = true;
         console.log('Connected to MongoDB with Mongoose');
         callback();
+    } catch (err) {
+        console.log("MongoDB connection failed: ", err);
     }
-    catch(err){
-        console.log("MongoDB connection failded: ", err);
-    }
-}
+};
 
 exports.mongoose = mongoose;
 exports.mongoConnect = mongoConnect;
